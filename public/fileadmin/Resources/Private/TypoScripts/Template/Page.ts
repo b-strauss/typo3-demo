588828d6359bdbbbfab15513e33940c350f7b6bb
Benjamin Strauß <b.strauss@netzbewegung.com>
2017-01-11 16:54:56 +0100

    update page

--- public/fileadmin/Resources/Private/TypoScripts/Template/Page.ts
+++ public/fileadmin/Resources/Private/TypoScripts/Template/Page.ts
@@ -70,16 +70,16 @@ page {
     60 = TEXT
     60.value (
       <script>
-        var $buoop = {vs: {i: 10, f: -4, o: -4, s: 7, c: -4}, c: 4};
+        var $buoop = {vs: {i: 10, f: -4, o: -4, s: 8, c: -4}, c: 4};
         function $buo_f() {
-          var e = document.createElement("script");
-          e.src = "//browser-update.org/update.min.js";
-          document.body.appendChild(e);
+          var el = document.createElement('script');
+          el.src = 'https://browser-update.org/update.min.js';
+          document.body.appendChild(el);
         }
         try {
-          document.addEventListener("DOMContentLoaded", $buo_f, false);
+          document.addEventListener('DOMContentLoaded', $buo_f, false);
         } catch (e) {
-          window.attachEvent("onload", $buo_f);
+          window.attachEvent('onload', $buo_f);
         }
       </script>
     )
